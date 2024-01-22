f2dc4a608bdf424aff6dc747441effd5358482d1
Sankioppro <141431228+Sankioppro@users.noreply.github.com>
2024-01-23 01:25:48 +0530

    Update userbot.py

--- Romeo/core/userbot.py
+++ Romeo/core/userbot.py
@@ -53,8 +53,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("RomeoBot_op")
-                await self.one.join_chat("Romeo_op")
+                await self.one.join_chat("Classics0012")
+                await self.one.join_chat("heartbeatbo")
             except:
                 pass
             assistants.append(1)
@@ -84,8 +84,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("RomeoBot_op")
-                await self.two.join_chat("Romeo_op")
+                await self.two.join_chat("Classics0012")
+                await self.two.join_chat("heartbeatbo")
             except:
                 pass
             assistants.append(2)
@@ -115,8 +115,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("RomeoBot_op")
-                await self.three.join_chat("Romeo_op")
+                await self.three.join_chat("Classics0012")
+                await self.three.join_chat("heartbeatbo")
             except:
                 pass
             assistants.append(3)
@@ -146,8 +146,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("RomeoBot_op")
-                await self.four.join_chat("Romeo_op")
+                await self.four.join_chat("Classics0012")
+                await self.four.join_chat("heartbeatbo")
             except:
                 pass
             assistants.append(4)
@@ -177,8 +177,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("RomeoBot_op")
-                await self.five.join_chat("Romeo_op")
+                await self.five.join_chat("Classics0012")
+                await self.five.join_chat("heartbeatbo")
             except:
                 pass
             assistants.append(5)
